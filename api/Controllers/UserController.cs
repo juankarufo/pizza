ccb7b6ea2865632b2fb88c4a0640b56157ce03d6
Juan Carlos de la Coba <rufo4evercrack@gmail.com>
2021-01-18 21:18:40 +0100

    user login model

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Flavours = new[]
+        {
+            "Pepperoni", "Carbonara", "Margarita", "Marina", "Roquefort"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new User
+            {
+                
+            })
+            .ToArray();
+        }
+        
+        [HttpPost("login")]
+        public User AuthenticateUser( [FromBody] User bodyPost)
+        {
+            return bodyPost;
+        }
+    }
+}
---
+++ api/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace api
+{
+    public class User
+    {
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
